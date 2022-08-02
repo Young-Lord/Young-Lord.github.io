0d96200b042bc9248b8fb9c8c73324b84c021e6a
Young-Lord <51789698+Young-Lord@users.noreply.github.com>
2022-08-02 21:23:46 +0800

    小重构

--- assets/js/jekyll_table_of_contents.js
+++ assets/js/jekyll_table_of_contents.js
@@ -1,3 +1,8 @@
 document.write('<div id="toc"></div>');
-document.write('<script src="//cdn.jsdelivr.net/gh/ghiculescu/jekyll-table-of-contents/toc.min.js"></script>');
-document.addEventListener("DOMContentLoaded", function () {$('#toc').toc();});
+(function () {
+    var SOURCES = window.TEXT_VARIABLES.sources;
+    window.Lazyload.js(SOURCES.jquery, function () {
+        document.write('<script src="//cdn.jsdelivr.net/gh/ghiculescu/jekyll-table-of-contents/toc.min.js"></script>');
+        document.addEventListener("DOMContentLoaded", function () { $('#toc').toc(); });
+    })
+})();
