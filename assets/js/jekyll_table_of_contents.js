7f48603b0a808aaef2f189acae34362f3b55e6c9
Young-Lord <51789698+Young-Lord@users.noreply.github.com>
2022-09-11 08:19:19 +0800

    移除 docs & 更新 toc 来源

--- assets/js/jekyll_table_of_contents.js
+++ assets/js/jekyll_table_of_contents.js
@@ -2,7 +2,7 @@
     var SOURCES = window.TEXT_VARIABLES.sources;
     window.Lazyload.js(SOURCES.jquery, function () {
         var JSElement=document.createElement("script");
-        JSElement.setAttribute("src","//cdn.jsdelivr.net/gh/ghiculescu/jekyll-table-of-contents/toc.min.js");
+        JSElement.setAttribute("src","//cdn.jsdelivr.net/gh/Young-Lord/Young-Lord.github.io/assets/js/jekyll-table-of-contents/toc.min.js");
         document.body.appendChild(JSElement)
         document.addEventListener("DOMContentLoaded", function () { $('#toc').toc(); });
     })
